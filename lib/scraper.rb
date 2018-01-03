b4b66c4994af7d109bb2f6070cded9421cd1d226
Emily Zhao <tong.zhao.92@gmail.com>
2018-01-03 17:41:19 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -18,9 +18,8 @@ class Scraper
 
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+
+    binding.pry
   end
-  
+
 end
-
-
-
